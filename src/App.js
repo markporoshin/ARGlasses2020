ad78da940016f3446c502d01c94802f37a679401
markporoshin <mark.poroshin@yandex.ru>
2020-02-22 13:33:31 +0300

    add connection between children components

--- src/App.js
+++ src/App.js
@@ -4,29 +4,60 @@ import './App.css';
 import FaceSelector from './components/FaceSelector';
 import GlassesSelector from './components/GlassesSelector';
 import Canvas from './components/Canvas'
+import {loadModels} from './faceapi'
 
-function App() {
-  return (
-    <Container fluid>
-      <Row style={{ backgroundColor: 'lightblue', height: '100vh' }}>
-        
-        <Col sm='3' style={{ backgroundColor: 'lightblue', height: '100%' }}>
-          <Row style={{ backgroundColor: 'lightblue', height: '100%' }}>
-            <FaceSelector/>
-          </Row>
-        </Col>
-        
-        <Col sm='6' style={{ backgroundColor: 'lightblue', height: '100%' }}>
-          <Canvas/>
-        </Col>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedFace: null, 
+      selectedGlasses: null,
+      isModelsLoaded: false,
+    }
+  
+  }
+
+  async componentDidMount() {
+    await loadModels();
+    this.setState({isModelsLoaded: true})
+  }
+
+  faceWasSelected(face) {
+    this.setState({selectedFace: face})
+  }
+
+  glassesWasSelected(glasses) {
+    this.setState({selectedGlasses: glasses})
+  }
+
+  render() {
+    let canvas = <Canvas face={this.state.selectedFace} 
+                         glasses={this.state.selectedGlasses}
+                         isModelsLoaded={this.state.isModelsLoaded}/>
+
+    return (
+      <Container fluid>
+        <Row style={{ backgroundColor: 'lightblue', height: '100vh' }}>
           
-        <Col sm='3' style={{ backgroundColor: 'lightblue', height: '100%' }}>
-          <GlassesSelector/>
-        </Col>
+          <Col sm='3' style={{ backgroundColor: 'lightblue', height: '100%' }}>
+            <Row style={{ backgroundColor: 'lightblue', height: '100%' }}>
+              <FaceSelector faceCallback={this.faceWasSelected.bind(this)}/>
+            </Row>
+          </Col>
+          
+          <Col sm='6' style={{ backgroundColor: 'lightblue', height: '100%' }}>
+            {canvas}
+          </Col>
+            
+          <Col sm='3' style={{ backgroundColor: 'lightblue', height: '100%' }}>
+            <GlassesSelector glassesCallback={this.glassesWasSelected.bind(this)}/>
+          </Col>
 
-      </Row>
-    </Container>
-  );
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
