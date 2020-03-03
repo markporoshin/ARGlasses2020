0e6cfd91b173049f43ec7654d83341a09883997e
markporoshin <mark.poroshin@yandex.ru>
2020-03-03 12:08:43 +0300

    zero version

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -15,50 +15,73 @@ function drawFace(state) {
     let faceImg = state.face
 
 
-    //real size in pixels
-    var clientWidth = ctx.canvas.clientWidth;
-    var clientHeight = ctx.canvas.clientHeight;
+    // real size in pixels
+    // var clientWidth = ctx.canvas.clientWidth;
+    // var clientHeight = ctx.canvas.clientHeight;
 
     //canvas coords
     var wCanvas = ctx.canvas.width;
     var hCanvas = ctx.canvas.height;
 
-    const wImage = faceImg.width;
-    const hImage = faceImg.height;
+    const wFace = faceImg.width;
+    const hFace = faceImg.height;
 
-    let kx = wCanvas / wImage;
-    let ky = hCanvas / hImage;
+    let kx = wCanvas / wFace;
+    let ky = hCanvas / hFace;
 
     let k = (kx) < (ky) ? (kx) : (ky);
 
-    ctx.drawImage(faceImg, 0, 0, wImage * k, hImage * k);
+    ctx.drawImage(faceImg, 0, 0, wFace * k, hFace * k);
 
-    const positions = fullDesc[0].landmarks._positions;
+    if (state.fullDesc != null) {
+        clearInterval(state.onTimeId)
 
-    const indicesEyeLeft = [36, 37, 38, 39, 40, 41, 36];
-    ctx.beginPath();
-    for (i = 0; i < indicesEyeLeft.length; i++)
-    {
-      const p = positions[indicesEyeLeft[i]];
-      if (i === 0)
-        ctx.moveTo(p._x * k, p._y * k);
-      else
-        ctx.lineTo(p._x * k, p._y * k);
+        const positions = fullDesc[0].landmarks._positions;
+        if (state.isGlassesLoad) {
+            clearInterval(state.onGlassesId)
+            let glasses = state.glasses;
+
+            let leftPoint = positions[37];
+            let rightPoint = positions[46];
+
+            let centerX = (leftPoint._x + rightPoint._x) / 2
+            let centerY = (leftPoint._y + rightPoint._y) / 2
+
+            const wGlassesImg = glasses.width;
+            const hGlassesImg = glasses.height;
+
+            let wGlasses = positions[17]._x - positions[1]._x;
+            let hGlasses = hGlassesImg * wGlasses / wGlassesImg
+
+            let kGl = (wGlasses / wCanvas) * (wCanvas / wGlassesImg)
+
+            ctx.drawImage(glasses, (centerX - wGlasses / 2) * kGl, (centerY - hGlasses / 2) * kGl, wGlasses * kGl, hGlasses * kGl)
+        }
+        const indicesEyeLeft = [36, 37, 38, 39, 40, 41, 36];
+        ctx.beginPath();
+        for (i = 0; i < indicesEyeLeft.length; i++)
+        {
+        const p = positions[indicesEyeLeft[i]];
+        if (i === 0)
+            ctx.moveTo(p._x * k, p._y * k);
+        else
+            ctx.lineTo(p._x * k, p._y * k);
+        }
+        ctx.stroke();
+
+        // draw eye right
+        const indicesEyeRight = [42, 43, 44, 45, 46, 47, 42];
+        ctx.beginPath();
+        for (i = 0; i < indicesEyeRight.length; i++)
+        {
+        const p = positions[indicesEyeRight[i]];
+        if (i === 0)
+            ctx.moveTo(p._x * k, p._y * k);
+        else
+            ctx.lineTo(p._x * k, p._y * k);
+        }
+        ctx.stroke();
     }
-    ctx.stroke();
-
-    // draw eye right
-    const indicesEyeRight = [42, 43, 44, 45, 46, 47, 42];
-    ctx.beginPath();
-    for (i = 0; i < indicesEyeRight.length; i++)
-    {
-      const p = positions[indicesEyeRight[i]];
-      if (i === 0)
-        ctx.moveTo(p._x * k, p._y * k);
-      else
-        ctx.lineTo(p._x * k, p._y * k);
-    }
-    ctx.stroke();
 }
 
 
@@ -70,6 +93,7 @@ class Canvas extends React.Component {
         super(props)
         this.state = {
             isModelsLoaded: false,
+            isGlassesLoad: false,
             fullDesc: null,
             onTimeId: null,
             canvas: null
@@ -77,15 +101,11 @@ class Canvas extends React.Component {
     }
 
     onTime() {
-        if (this.state.fullDesc != null) {
-
-            clearInterval(this.state.onTimeId)
-            this.setState({
-                onTimeId: null, 
-                canvas: this.refs.canvas
-            })
-            drawFace(this.state)
-        }
+        this.setState({
+            onTimeId: null, 
+            canvas: this.refs.canvas
+        })
+        drawFace(this.state)
     }
 
     async componentDidMount() {
@@ -106,18 +126,34 @@ class Canvas extends React.Component {
             const imgSrc = this.refs.face
             this.setState({imgH: imgSrc.height, imgW: imgSrc.width})
             this.setState({face: imgSrc})
-            
+
+            this.setState({
+                onTimeId: null, 
+                canvas: this.refs.canvas
+            })
+            //drawFace(this.state)
+            this.setState({
+                onTimeId: setInterval(this.onTime.bind(this), 50)
+            })
             if (this.props.faceNumber != null
                 && this.state.isModelsLoaded) {
                 
                 this.setState({fullDesc: await getLandmarks(imgSrc)})
             }
-            this.setState({
-                onTimeId: setInterval(this.onTime.bind(this), 50)
-            })
+            
         } catch (err) {
             console.log(err);
         }
+    }
+
+    async onLoadGlasses() {
+        this.setState({
+            glasses: this.refs.glasses, 
+            isGlassesLoad: true
+        }).then(
+            drawFace(this.state)
+        )
+        
     }
 
     render() {
@@ -125,12 +161,11 @@ class Canvas extends React.Component {
             display: 'none'
         };
 
-        //<canvas style={{width:'100%',height:'100%'}} ref='canvas'/>
-
         return (
             <Row fluid style={{height: '100%'}}>
                 <Col style={{height: '100%'}}>
                     <img src={faces[this.props.faceNumber]} onLoad={this.onLoadFace.bind(this)} style={styleImage} ref='face' alt='face'/>
+                    <img src={glasses[this.props.glassesNumber]} onLoad={this.onLoadGlasses.bind(this)} style={styleImage} ref='glasses' alt='glasses'/>
                     <div>{this.props.faceNumber}</div>
                     <div>{this.props.glassesNumber}</div>
                     <canvas style={{width:'100%',height:'100%'}} ref='canvas'/>
