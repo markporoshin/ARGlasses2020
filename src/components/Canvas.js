54a4ca9cc5bdf423536e00051dbe046894a99df9
markporoshin <mark.poroshin@yandex.ru>
2020-03-04 12:19:24 +0300

    Add glasses covering

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import {getLandmarks, loadModels} from '../faceapi'
-import {faces, glasses} from '../images'
+import {faces, glasses} from '../resource'
 import {Col, Container,Row} from 'react-bootstrap';
+
+
+function rotate(point, angle) {
+    return {
+        _x: point._x * Math.cos(angle) + point._y * Math.sin(angle),
+        _y: point._y * Math.cos(angle) - point._x * Math.sin(angle)
+    }
+}
+
 
 
 function drawFace(state) {
@@ -29,9 +38,9 @@ function drawFace(state) {
     let kx = wCanvas / wFace;
     let ky = hCanvas / hFace;
 
-    let k = (kx) < (ky) ? (kx) : (ky);
+    let kImg = (kx) < (ky) ? (kx) : (ky);
 
-    ctx.drawImage(faceImg, 0, 0, wFace * k, hFace * k);
+    ctx.drawImage(faceImg, 0, 0, wFace * kImg, hFace * kImg);
 
     if (state.fullDesc != null) {
         clearInterval(state.onTimeId)
@@ -41,31 +50,69 @@ function drawFace(state) {
             clearInterval(state.onGlassesId)
             let glasses = state.glasses;
 
-            let leftPoint = positions[37];
-            let rightPoint = positions[46];
+            let leftPoint = positions[36];
+            let rightPoint = positions[45];
+
+            let angle = Math.atan((rightPoint._y - leftPoint._y) / (rightPoint._x - leftPoint._x))
+            console.log('angle: ' + angle)
+            
 
             let centerX = (leftPoint._x + rightPoint._x) / 2
             let centerY = (leftPoint._y + rightPoint._y) / 2
 
+            
+
             const wGlassesImg = glasses.width;
             const hGlassesImg = glasses.height;
 
-            let wGlasses = positions[17]._x - positions[1]._x;
+            let wGlasses = positions[16]._x - positions[0]._x;
             let hGlasses = hGlassesImg * wGlasses / wGlassesImg
 
-            let kGl = (wGlasses / wCanvas) * (wCanvas / wGlassesImg)
 
-            ctx.drawImage(glasses, (centerX - wGlasses / 2) * kGl, (centerY - hGlasses / 2) * kGl, wGlasses * kGl, hGlasses * kGl)
+            let x = (centerX * kImg - wGlasses / 2 * kImg)
+            let y = (centerY * kImg - hGlasses / 2 * kImg)
+
+            ctx.translate(x, y)
+            ctx.rotate(angle)
+            ctx.drawImage(glasses, 0, 0, wGlasses * kImg, hGlasses * kImg)
+            ctx.rotate(-angle)
+            ctx.translate(-x, -y)
+            
+
+            ctx.beginPath();
+            ctx.moveTo(leftPoint._x* kImg, leftPoint._y * kImg);
+            ctx.lineTo(rightPoint._x * kImg, rightPoint._y * kImg);
+            ctx.stroke();
+
+            // ctx.beginPath();
+            // ctx.moveTo(x, y);
+            // ctx.lineTo(x + wGlasses * kImg, y);
+            // ctx.lineTo(x + wGlasses * kImg, y + hGlasses * kImg);
+            // ctx.lineTo(x, y + hGlasses * kImg);
+            // ctx.lineTo(x, y);
+            // ctx.stroke();
         }
+
+        ctx.beginPath();
+        for (i = 0; i < 17; i++)
+        {
+        const p = positions[i];
+        if (i === 0)
+            ctx.moveTo(p._x * kImg, p._y * kImg);
+        else
+            ctx.lineTo(p._x * kImg, p._y * kImg);
+        }
+        ctx.stroke();
+
         const indicesEyeLeft = [36, 37, 38, 39, 40, 41, 36];
         ctx.beginPath();
         for (i = 0; i < indicesEyeLeft.length; i++)
         {
         const p = positions[indicesEyeLeft[i]];
         if (i === 0)
-            ctx.moveTo(p._x * k, p._y * k);
+            ctx.moveTo(p._x * kImg, p._y * kImg);
         else
-            ctx.lineTo(p._x * k, p._y * k);
+            ctx.lineTo(p._x * kImg, p._y * kImg);
         }
         ctx.stroke();
 
@@ -76,9 +123,9 @@ function drawFace(state) {
         {
         const p = positions[indicesEyeRight[i]];
         if (i === 0)
-            ctx.moveTo(p._x * k, p._y * k);
+            ctx.moveTo(p._x * kImg, p._y * kImg);
         else
-            ctx.lineTo(p._x * k, p._y * k);
+            ctx.lineTo(p._x * kImg, p._y * kImg);
         }
         ctx.stroke();
     }
@@ -130,15 +177,19 @@ class Canvas extends React.Component {
             this.setState({
                 onTimeId: null, 
                 canvas: this.refs.canvas
+            }, () => {
+                drawFace(this.state)
             })
             //drawFace(this.state)
-            this.setState({
-                onTimeId: setInterval(this.onTime.bind(this), 50)
-            })
+            // this.setState({
+            //     onTimeId: setInterval(this.onTime.bind(this), 50)
+            // })
             if (this.props.faceNumber != null
                 && this.state.isModelsLoaded) {
                 
-                this.setState({fullDesc: await getLandmarks(imgSrc)})
+                this.setState({fullDesc: await getLandmarks(imgSrc)}, () => {
+                    drawFace(this.state)
+                })
             }
             
         } catch (err) {
@@ -150,9 +201,7 @@ class Canvas extends React.Component {
         this.setState({
             glasses: this.refs.glasses, 
             isGlassesLoad: true
-        }).then(
-            drawFace(this.state)
-        )
+        }, ()=>{drawFace(this.state)})
         
     }
 
