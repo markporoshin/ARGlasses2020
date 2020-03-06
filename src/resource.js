dd74f0de4a8480575afb5e6751ad7e4a03884841
markporoshin <mark.poroshin@yandex.ru>
2020-03-06 18:12:56 +0300

    fix arrays

--- src/resource.js
+++ src/resource.js
@@ -2,10 +2,10 @@ import Face1 from './images/faces/face1.jpg'
 import Face2 from './images/faces/face2.jpg'
 import Face3 from './images/faces/face3.jpg'
 import Face4 from './images/faces/face4.jpg'
-import Face5 from './images/faces/face5.jpg'
-import Face6 from './images/faces/face6.jpg'
-import Face7 from './images/faces/face7.jpg'
-import Face8 from './images/faces/face8.jpg'
+// import Face5 from './images/faces/face5.jpg'
+// import Face6 from './images/faces/face6.jpg'
+// import Face7 from './images/faces/face7.jpg'
+// import Face8 from './images/faces/face8.jpg'
 //import Face9 from './images/faces/face9.jpg'
 
 import Glasses1 from './images/glasses/gl1.png'
@@ -13,12 +13,11 @@ import Glasses2 from './images/glasses/gl2.png'
 import Glasses3 from './images/glasses/gl3.png'
 import Glasses4 from './images/glasses/gl4.png'
 import Glasses5 from './images/glasses/gl5.png'
-import Glasses6 from './images/glasses/gl6.png'
 
 
 
-export const faces = [Face1, Face2, Face3, Face4, Face5, Face6, Face7, Face8,/*Face9,*/]
-export const glasses = [Glasses1, Glasses2, Glasses3, Glasses4, Glasses5, Glasses6]
+export const faces = [Face1, Face2, Face3, Face4/*, Face5, Face6, Face7, Face8,Face9,*/]
+export const glasses = [Glasses1, Glasses2, Glasses3, Glasses4, Glasses5/*, Glasses6*/]
 
 export const strings = {
     'RU' : {
