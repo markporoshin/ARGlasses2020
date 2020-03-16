750ab60ef690cce536291938d982624ac67b1a73
kate <zabolotskihkate@gmail.com>
2020-03-16 23:42:43 +0300

    update res

--- src/resource.js
+++ src/resource.js
@@ -2,10 +2,6 @@ import Face1 from './images/faces/face1.jpg'
 import Face2 from './images/faces/face2.jpg'
 import Face3 from './images/faces/face3.jpg'
 import Face4 from './images/faces/face4.jpg'
-// import Face5 from './images/faces/face5.jpg'
-// import Face6 from './images/faces/face6.jpg'
-// import Face7 from './images/faces/face7.jpg'
-// import Face8 from './images/faces/face8.jpg'
 //import Face9 from './images/faces/face9.jpg'
 
 import Glasses1 from './images/glasses/gl1.png'
@@ -14,18 +10,26 @@ import Glasses3 from './images/glasses/gl3.png'
 import Glasses4 from './images/glasses/gl4.png'
 import Glasses5 from './images/glasses/gl5.png'
 
+import rim5 from './images/glasses/rim5.png'
+import lins5 from './images/glasses/lins5.png'
+
 
 
 export const faces = [Face1, Face2, Face3, Face4/*, Face5, Face6, Face7, Face8,Face9,*/]
-export const glasses = [Glasses1, Glasses2, Glasses3, Glasses4, Glasses5/*, Glasses6*/]
+export const glasses = [/*Glasses1, Glasses2, Glasses3, Glasses4, */Glasses5/*, Glasses6*/]
+export const rimsCenter = [0.4]
+export const rims = [rim5]
+export const lins = [lins5]
 
 export const strings = {
     'RU' : {
         'greeting' : 'привет незнакомец',
-        'APPName' : 'AR примерка'
+        'APPName' : 'AR примерка',
+        'alpha' : 'прозрачность линз',
     },
     'EN' : {
         'greeting' : 'hello anonym',
-        'APPName' : 'ARGlasses2020'
+        'APPName' : 'ARGlasses2020',
+        'alpha' : 'lins\'s alpha'
     }
 }
