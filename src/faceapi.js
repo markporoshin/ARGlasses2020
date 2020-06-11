a962bb80f6a946de54488f37babb2ac3813f640c
NikitaAniskov <aniskoff.nick@gmail.com>
2020-06-12 02:50:45 +0300

    change faceapi setup

--- src/faceapi.js
+++ src/faceapi.js
@@ -5,36 +5,25 @@
 import * as faceapi from 'face-api.js';
 
 // ********************************************************
-// Const
-// ********************************************************
-
-const USE_TINY_MODEL = true;
-
-// ********************************************************
 // Class
 // ********************************************************
 
 export async function loadModels() {
   console.log('face-api create models...');
   const MODEL_URL = process.env.PUBLIC_URL + '/weights';
-  await faceapi.loadTinyFaceDetectorModel(MODEL_URL); 
-  await faceapi.loadFaceLandmarkTinyModel(MODEL_URL);
-  await faceapi.loadFaceRecognitionModel(MODEL_URL);
+  await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
+  await faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL);
+  await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
   console.log('face-api loaded models!');
 }
 
 export async function getLandmarks(imageSrc) {
-  const wImage = imageSrc.width;
-  let scoreThreshold = 0.5;
-  const options = new faceapi.TinyFaceDetectorOptions({
-    wImage,
-    scoreThreshold
-  });
-  console.log('face-api start detect face...');
-  let fullDesc = await faceapi.detectAllFaces(imageSrc, options)
-    .withFaceLandmarks(USE_TINY_MODEL)
-    .withFaceDescriptors();
-  console.log('face-api completed face detection.');    
+  let img = await faceapi.fetchImage(imageSrc);
+  console.log('face-api start detect face...')
+  let fullDesc = await  faceapi
+      .detectAllFaces(img)
+      .withFaceLandmarks();
+  console.log('face-api completed face detection.');
   return fullDesc;
 }
 
